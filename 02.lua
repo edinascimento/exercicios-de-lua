f6efccd6f924dacaf82a1da68d23e88d08372f6a
Edipo Nascimento <esnedipo@gmail.com>
2024-05-28 10:39:55 -0300

    Programa para exibir um numero recebido via stdin

---
+++ 02.lua
@@ -0,0 +1,8 @@
+--[[
+    Faça um programa que peça um número e então mostre a mensagem
+    o número digitado foi (numero)
+]]--
+
+print("Digite um número: ")
+local numero = tonumber(io.read());
+print("O númeo digitado foi: "..numero)
